447d6c2a8e51b45cb393653f399203533731e7b3
Spartak <spartaklysman@gmail.com>
2024-06-13 12:47:35 +0200

    CarServie. Security added

--- CarService/src/main/java/ua/com/foxminded/carService/config/SecurityConfig.java
+++ CarService/src/main/java/ua/com/foxminded/carService/config/SecurityConfig.java
@@ -1,5 +1,41 @@
 package ua.com.foxminded.carService.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	@Value("${auth0.audience}")
+	private String audience;
+
+	@Value("${auth0.issuer}")
+	private String issuer;
+
+	protected void configure(HttpSecurity http) throws Exception {
+		JwtWebSecurityConfigurer.forRS256(audience, issuer).configure(http).authorizeRequests()
+				.requestMatchers(HttpMethod.GET, "/api/v1/cars/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/manufacturers/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
+				.requestMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+				.requestMatchers(HttpMethod.PUT, "/api/v1/**").authenticated()
+				.requestMatchers(HttpMethod.DELETE, "/api/v1/**").authenticated().anyRequest().authenticated();
+	}
 }
+
+// http.authorizeRequests().requestMatchers(HttpMethod.GET,
+// "/api/menu/items/**").permitAll() // GET requests don't																						
+//				.anyRequest().authenticated().and().oauth2ResourceServer().jwt();
+//	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		JwtWebSecurityConfigurer.forRS256(audience, issuer).configure(http).authorizeRequests()
+//				.antMatchers(HttpMethod.GET, "/api/**").permitAll().antMatchers(HttpMethod.POST, "/api/**")
+//				.authenticated().anyRequest().authenticated();
+//	}
---
+++ CarService/src/main/java/ua/com/foxminded/carService/security/AudienceValidator.java
@@ -0,0 +1,34 @@
+package ua.com.foxminded.carService.security;
+//
+
+public class AudienceValidator {
+
+}
+//import org.springframework.security.oauth2.core.OAuth2Error;
+//import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+//import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+//import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//
+//import java.util.List;
+//import java.util.Objects;
+//
+//public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+//
+//	private final String audience;
+//
+//	  AudienceValidator(String audience) {
+//	    Assert.hasText(audience, "audience is null or empty");
+//	    this.audience = audience;
+//	  }
+//
+//	  public OAuth2TokenValidatorResult validate(Jwt jwt) {
+//	    List<String> audiences = jwt.getAudience();
+//	    if (audiences.contains(this.audience)) {
+//	      return OAuth2TokenValidatorResult.success();
+//	    }
+//	    OAuth2Error err = new OAuth2Error(OAuth2ErrorCodes.INVALID_TOKEN);
+//	    return OAuth2TokenValidatorResult.failure(err);
+//	  }
+//	}
+//}
