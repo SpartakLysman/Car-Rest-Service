2bcbea1d6caa62408748be2d4f522511da9771fb
Spartak <spartaklysman@gmail.com>
2024-06-25 18:23:48 +0200

    CarService. OpenApi V3 finished

--- CarService/src/main/java/ua/com/foxminded/carService/config/OpenApiConfig.java
+++ CarService/src/main/java/ua/com/foxminded/carService/config/OpenApiConfig.java
@@ -6,12 +6,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Car-rest-service", version = "v1"), servers = {
+@OpenAPIDefinition(info = @Info(title = "Car-rest-service", version = "v1"), security = @SecurityRequirement(name = "bearerAuth"), servers = {
 		@Server(description = "develop", url = "http://localhost:8080/car-rest-service/") })
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
+
+//	@Bean
+//	public OpenAPI customOpenAPI() {
+//		return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+//	}
 }
